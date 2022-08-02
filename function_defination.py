68e0993bf5bb591f6985f1b3ddc9096c0881cc98
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-08-02 13:06:03 +0530

    Add files via upload

---
+++ function_defination.py
@@ -0,0 +1,11 @@
+def sum (a,b):
+    c=a+b
+    print('sum =',c)
+def sub m(a,b):
+    d=a-b
+    e=a*b
+    print('sub=',d,'mul=',e)
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+sum(a,b)
+sub m(a,b)
